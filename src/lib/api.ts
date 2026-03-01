271092d27a16bfcbc1f429506668800929c95515
miguel <schizoid07@gmail.com>
2026-03-01 01:15:22 -0400

    corregido ky

---
+++ src/lib/api.ts
@@ -0,0 +1,31 @@
+import ky from 'ky';
+
+export const api = ky.create({
+
+    prefixUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+    // Tiempo máximo de espera para la respuesta
+    timeout: 10000,
+
+    // Configuraciones comunes
+    headers: {
+        'Content-Type': 'application/json',
+    },
+
+    // Manejo de errores automático
+    hooks: {
+        beforeRequest: [
+            request => {
+                // Útil para debug: verás en la consola del navegador cada petición
+                console.log(`Enviando petición a: ${request.url}`);
+            }
+        ],
+        afterResponse: [
+            async (_request, _options, response) => {
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    console.error('Error de la API:', errorData);
+                }
+            }
+        ]
+    }
+});
